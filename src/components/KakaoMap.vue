14669a8a45d428bb0526b94366738b03023cc665
jw <betrayers000@gmail.com>
2020-01-19 18:24:38 +0900

    infowindow modify

--- src/components/KakaoMap.vue
+++ src/components/KakaoMap.vue
@@ -50,7 +50,7 @@ export default {
                         clickable : true,
                     })
                     nMarker.setMap(map)
-                    var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+                    var iwContent = `<div style="padding:5px;">${marker.content}</div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
                     iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
                     // 인포윈도우를 생성합니다
                     var infowindow = new kakao.InfoWindow({
