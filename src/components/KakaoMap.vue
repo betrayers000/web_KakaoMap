925c591b4090c5062b00733fc682e0a83e6e042a
jw <betrayers000@gmail.com>
2020-01-20 20:21:22 +0900

    kakaomap overlay add

--- src/components/KakaoMap.vue
+++ src/components/KakaoMap.vue
@@ -41,39 +41,56 @@ export default {
                     info = []
                 }
             })
-            // create marker
-            if (markers != null){
+            //create overlay
+            if (markers != null) {
                 markers.forEach(function(marker){
+                    var content = '<div style="display:table-cell; background-color:white; border-radius:10px; width:70px; height:30px; text-align:center; vertical-align:middle;">' + 
+                        `<span>${marker.content}</span>` + 
+                        '</div>'
                     var markerPosition = new kakao.LatLng(marker.lat, marker.lng)
-                    var nMarker = new kakao.Marker({
+                    var customOverlay = new kakao.CustomOverlay({
                         position : markerPosition,
-                        clickable : true,
+                        content : content,
+                        xAnchor : 0.3,
+                        yAnchor : 0.91
                     })
-                    nMarker.setMap(map)
-                    var iwContent = `<div style="padding:5px;">${marker.content}</div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-                    iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
-                    // 인포윈도우를 생성합니다
-                    var infowindow = new kakao.InfoWindow({
-                        content : iwContent,
-                        removable : iwRemoveable
-                    });
-                    // 마커에 클릭이벤트를 등록합니다
-                    kakao.event.addListener(nMarker, 'click', function() {
-                        // 마커 위에 인포윈도우를 표시합니다
-                        console.log(this.mapInfo)
-                        if (info.length != 0){
-                            info[0].close()
-                            info = []
-                        }
-                        info.push(infowindow)
-                        info[0].open(map, nMarker);
-                        this.mapInfo = info 
-                    });
+                    customOverlay.setMap(map)
                 })
             }
-            else {
-                console.log(map)
-            }
+
+            // create marker
+            // if (markers != null){
+            //     markers.forEach(function(marker){
+            //         var markerPosition = new kakao.LatLng(marker.lat, marker.lng)
+            //         var nMarker = new kakao.Marker({
+            //             position : markerPosition,
+            //             clickable : true,
+            //         })
+            //         nMarker.setMap(map)
+            //         var iwContent = `<div style="padding:5px;">${marker.content}</div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+            //         iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+            //         // 인포윈도우를 생성합니다
+            //         var infowindow = new kakao.InfoWindow({
+            //             content : iwContent,
+            //             removable : iwRemoveable
+            //         });
+            //         // 마커에 클릭이벤트를 등록합니다
+            //         kakao.event.addListener(nMarker, 'click', function() {
+            //             // 마커 위에 인포윈도우를 표시합니다
+            //             console.log(this.mapInfo)
+            //             if (info.length != 0){
+            //                 info[0].close()
+            //                 info = []
+            //             }
+            //             info.push(infowindow)
+            //             info[0].open(map, nMarker);
+            //             this.mapInfo = info 
+            //         });
+            //     })
+            // }
+            // else {
+            //     console.log(map)
+            // }
         },
     },
     mounted(){
